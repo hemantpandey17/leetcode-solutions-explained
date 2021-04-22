00a62d83a62efad2fa1e9c245ce039f3257bbe4a
Hemant Pandey <hemant.pandey17@gmail.com>
2021-04-21 17:01:33 -0700

    Create 217-Contains-Duplicate.java

---
+++ 217-Contains-Duplicate.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/contains-duplicate/
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>(nums.length);
+        for (int x: nums) {
+            if (!set.add(x))
+                return true;
+        }
+        return false;
+   }
+}
