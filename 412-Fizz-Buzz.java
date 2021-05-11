decacc0f1961890e7b6b49fe3aed34ef01a87721
Hemant Pandey <hemant.pandey17@gmail.com>
2021-05-11 16:33:16 -0700

    Create 412-Fizz-Buzz.java

---
+++ 412-Fizz-Buzz.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> ls = new ArrayList<String>();
+        for(int i=1; i<=n ; i++)
+        {
+            if(i%3 == 0 && i%5 != 0)
+            ls.add("Fizz");
+            else if(i%5 ==0 && i% 3 != 0)
+            ls.add("Buzz");
+            else if(i%3 == 0 && i%5 == 0)
+            ls.add("FizzBuzz");
+            else 
+            ls.add(Integer.toString(i));
+        }
+        return ls;
+    }
+}
