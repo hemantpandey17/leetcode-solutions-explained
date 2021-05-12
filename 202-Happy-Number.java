15ab7d9dde59d799f00c680fc7bd8969650048c5
Hemant Pandey <hemant.pandey17@gmail.com>
2021-05-12 16:30:59 -0700

    Create 202-Happy-Number.java

---
+++ 202-Happy-Number.java
@@ -0,0 +1,24 @@
+public class Solution {
+ public boolean isHappy(int n) {
+    Set<Integer> inLoop = new HashSet<Integer>();
+    int squareSum,remain;
+	while (inLoop.add(n)) {
+	    System.out.println(inLoop);
+		squareSum = 0;
+		while (n > 0) {
+		    remain = n%10;
+			squareSum += remain*remain;
+			n /= 10;
+		}
+		
+		if (squareSum == 1)
+			return true;
+		else
+			n = squareSum;
+
+	}
+	
+	return false;
+
+}
+}
