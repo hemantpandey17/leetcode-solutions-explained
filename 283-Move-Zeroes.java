e8b59ab25ddabf0752d9235bd31db35d9b9bd92d
Hemant Pandey <hemant.pandey17@gmail.com>
2021-05-11 23:30:17 -0700

    Create 283-Move-Zeroes.java

---
+++ 283-Move-Zeroes.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int len  = nums.length;
+        int index = 0;
+        for(int i = 0; i<len; i++) {
+            if(nums[i] != 0) {
+                nums[index++] = nums[i];
+            }
+        }
+        while(index < len) {
+            nums[index++] = 0;
+        }
+    }
+}
